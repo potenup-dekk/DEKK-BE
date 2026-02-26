20190499384c156f04162bc2a56a041986e78f99
Sung Ryul Cho <hahahoho5811@gmail.com>
2026-02-26 20:13:41 +0900

    feat(activeLog) : 중복 스와이프 관련 도메인 예외 코드 추가

--- src/main/java/com/dekk/activelog/domain/exception/ActiveLogErrorCode.java
+++ src/main/java/com/dekk/activelog/domain/exception/ActiveLogErrorCode.java
@@ -9,7 +9,10 @@ public enum ActiveLogErrorCode implements ErrorCode {
     CARD_ID_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EAL40002", "카드 ID는 필수 값입니다"),
     SWIPE_TYPE_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EAL40003", "스와이프 타입은 필수 값입니다"),
 
-    ACTIVE_LOG_NOT_FOUND(HttpStatus.NOT_FOUND, "EAL40401", "스와이프 이력을 찾을 수 없습니다");
+    ACTIVE_LOG_NOT_FOUND(HttpStatus.NOT_FOUND, "EAL40401", "스와이프 이력을 찾을 수 없습니다"),
+
+    ALREADY_SWIPED(HttpStatus.CONFLICT, "EAL40901", "이미 스와이프 평가를 완료한 카드입니다."),
+    ;
 
     private final HttpStatus httpStatus;
     private final String code;

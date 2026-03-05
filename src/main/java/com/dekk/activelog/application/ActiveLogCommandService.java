437e159debbf1f1ed711e46a810378aa6362c37a
Sung Ryul Cho <hahahoho5811@gmail.com>
2026-03-05 10:37:13 +0900

    [DK-191] feat: 스와이프 LIKE 액션 시 기본 보관함 자동 저장 연동

--- src/main/java/com/dekk/activelog/application/ActiveLogCommandService.java
+++ src/main/java/com/dekk/activelog/application/ActiveLogCommandService.java
@@ -1,10 +1,10 @@
 package com.dekk.activelog.application;
 
 import com.dekk.activelog.application.dto.command.SwipeCommand;
-import com.dekk.activelog.domain.exception.ActiveLogBusinessException;
-import com.dekk.activelog.domain.exception.ActiveLogErrorCode;
 import com.dekk.activelog.domain.model.ActiveLog;
+import com.dekk.activelog.domain.model.SwipeType;
 import com.dekk.activelog.domain.repository.ActiveLogRepository;
+import com.dekk.deck.application.DeckCardCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ActiveLogCommandService {
 
     private final ActiveLogRepository activeLogRepository;
+    private final DeckCardCommandService deckCardCommandService;
 
     public void saveSwipeAction(SwipeCommand command) {
         if (activeLogRepository.existsByUserIdAndCardId(command.userId(), command.cardId())) {
@@ -23,5 +24,9 @@ public class ActiveLogCommandService {
 
         ActiveLog activeLog = ActiveLog.create(command.userId(), command.cardId(), command.swipeType());
         activeLogRepository.save(activeLog);
+
+        if(command.swipeType() == SwipeType.LIKE){
+            deckCardCommandService.saveToDefaultDeck(command.userId(), command.cardId());
+        }
     }
 }

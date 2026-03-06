3910bd326d5615e262f022e57111fd6ee38c821e
Sung Ryul Cho <hahahoho5811@gmail.com>
2026-03-06 12:22:34 +0900

    feat: [DK-216] 커스텀 보관함 조회 응답 코드 및 결과/Projection DTO 추가

---
+++ src/main/java/com/dekk/deck/infrastructure/jpa/DeckCardCountProjection.java
@@ -0,0 +1,6 @@
+package com.dekk.deck.infrastructure.jpa;
+
+public interface DeckCardCountProjection {
+    Long getDeckId();
+    Long getCardCount();
+}
--- src/main/java/com/dekk/deck/presentation/response/DeckResultCode.java
+++ src/main/java/com/dekk/deck/presentation/response/DeckResultCode.java
@@ -13,6 +13,7 @@ public enum DeckResultCode implements ResultCode {
     CUSTOM_DECK_CREATE_SUCCESS(HttpStatus.OK, "SD20005", "커스텀 보관함 생성 성공"),
     CUSTOM_DECK_UPDATE_SUCCESS(HttpStatus.OK, "SD20006", "커스텀 보관함 이름 수정 성공"),
     CUSTOM_DECK_DELETE_SUCCESS(HttpStatus.OK, "SD20007", "커스텀 보관함 삭제 성공"),
+    CUSTOM_DECK_LIST_SUCCESS(HttpStatus.OK, "SD20008", "커스텀 보관함 목록 조회 성공"),
     ;
 
     private final HttpStatus status;

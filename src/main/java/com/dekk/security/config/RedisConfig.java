d8ce9e3dee93720fa9697dc34311531492c274a1
youable <youable.framios@gmail.com>
2026-03-10 21:06:40 +0900

    hotfix: redis config 추가

--- src/main/java/com/dekk/security/config/RedisConfig.java
+++ src/main/java/com/dekk/security/config/RedisConfig.java
@@ -1,15 +1,34 @@
 package com.dekk.security.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Profile("!test")
 @Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration(host, port);
+
+        LettuceClientConfiguration clientConfig =
+                LettuceClientConfiguration.builder().useSsl().build();
+
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
+    }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {

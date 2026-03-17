cf4603f3d16b288688625540fa3c5bd0fc1b2c2f
youable <youable.framios@gmail.com>
2026-03-17 21:25:26 +0900

    refactor: code deploy 배포 스크립트 변경

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-PROJECT_ROOT="/home/ec2-user/dekk-was"
-JAR_FILE="$PROJECT_ROOT/build/libs/dekk-api-prod.jar" # 실제 빌드된 jar 파일 이름으로 변경하세요!
 
-CURRENT_PID=$(pgrep -f "dekk-api-prod.jar")
+CURRENT_PID=$(pgrep -f "dekk.*\.jar")
 
 if [ -z "$CURRENT_PID" ]; then
   echo "현재 실행 중인 애플리케이션이 없으므로 종료하지 않습니다."

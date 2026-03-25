0956bc3c8d401bdfbd4871f0551280c7e9d41ba0
youable <youable.framios@gmail.com>
2026-03-25 22:53:55 +0900

    refactor: CardResultCode 컨벤션에 맞게 수정

--- src/main/java/com/dekk/app/card/presentation/response/CardResultCode.java
+++ src/main/java/com/dekk/app/card/presentation/response/CardResultCode.java
@@ -6,10 +6,10 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum CardResultCode implements ResultCode {
-    GUEST_CARD_LIST_SUCCESS(HttpStatus.OK, "SC200001", "비회원 카드 목록 조회 성공"),
-    MEMBER_CARD_LIST_SUCCESS(HttpStatus.OK, "SC200002", "회원 카드 목록 조회 성공"),
-    CARD_APPROVE_SUCCESS(HttpStatus.OK, "SC200003", "카드 승인 성공"),
-    CARD_REJECT_SUCCESS(HttpStatus.OK, "SC200004", "카드 반려 성공"),
+    GUEST_CARD_LIST_SUCCESS(HttpStatus.OK, "SC200001", "비회원 카드 목록 조회에 성공했습니다."),
+    MEMBER_CARD_LIST_SUCCESS(HttpStatus.OK, "SC200002", "회원 카드 목록 조회에 성공했습니다."),
+    CARD_APPROVE_SUCCESS(HttpStatus.OK, "SC200003", "카드 승인에 성공했습니다."),
+    CARD_REJECT_SUCCESS(HttpStatus.OK, "SC200004", "카드 반려에 성공했습니다."),
     ADMIN_CARD_LIST_SUCCESS(HttpStatus.OK, "SC200005", "관리자 카드 목록 조회 성공 했습니다."),
     CARD_CATEGORIES_ASSIGNED(HttpStatus.OK, "SC200006", "카드 카테고리 지정에 성공했습니다."),
     ADMIN_CARD_DETAIL_SUCCESS(HttpStatus.OK, "SC200007", "관리자 카드 상세 조회에 성공했습니다."),

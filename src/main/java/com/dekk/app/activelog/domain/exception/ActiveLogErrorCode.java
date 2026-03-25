0737db4d2c64303fbcfcbe4aecf1a0f34121683c
youable <youable.framios@gmail.com>
2026-03-25 22:55:12 +0900

    refactor: ActiveLogErrorCode 컨벤션에 맞게 수정

--- src/main/java/com/dekk/app/activelog/domain/exception/ActiveLogErrorCode.java
+++ src/main/java/com/dekk/app/activelog/domain/exception/ActiveLogErrorCode.java
@@ -6,11 +6,11 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum ActiveLogErrorCode implements ErrorCode {
-    USER_ID_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EAL40001", "사용자 ID는 필수 값입니다"),
-    CARD_ID_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EAL40002", "카드 ID는 필수 값입니다"),
-    SWIPE_TYPE_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EAL40003", "스와이프 타입은 필수 값입니다"),
+    USER_ID_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EAL40001", "사용자 ID는 필수 값입니다."),
+    CARD_ID_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EAL40002", "카드 ID는 필수 값입니다."),
+    SWIPE_TYPE_IS_REQUIRED_TO_CREATE(HttpStatus.BAD_REQUEST, "EAL40003", "스와이프 타입은 필수 값입니다."),
 
-    ACTIVE_LOG_NOT_FOUND(HttpStatus.NOT_FOUND, "EAL40401", "스와이프 이력을 찾을 수 없습니다"),
+    ACTIVE_LOG_NOT_FOUND(HttpStatus.NOT_FOUND, "EAL40401", "스와이프 이력을 찾을 수 없습니다."),
 
     ALREADY_SWIPED(HttpStatus.CONFLICT, "EAL40901", "이미 스와이프 평가를 완료한 카드입니다."),
     ;
